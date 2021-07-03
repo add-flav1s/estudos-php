6d9b7b1f19f1ced816dd71ae1db5eb78025a0a5b
add-flav1s <aflavia17_@hotmail.com>
2021-07-03 16:09:27 -0300

    iniciando os estudos em php.

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+echo 'Ola mundo';
+
+/*Declarando variaveis.
+
+$name = 'Ana';
+$age = 27;
+$result = ($age >= 18) ? 'Você é maior de idade.' : 'Você é menor de idade.';
+echo $result;
+*/
+
+$person1 = [
+    'name' => 'Ana Flávia',
+    'age' => 27,
+    'trabalho' => true
+];
+
+
+$person2 = [
+    'name' => 'Agda Sousa',
+    'age' => 20,
+    'trabalho' => false
+];
