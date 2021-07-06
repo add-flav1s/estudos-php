75a45e067f9ee76d9314888a0302e0f5e2642638
add-flav1s <aflavia17_@hotmail.com>
2021-07-05 22:29:15 -0300

    docs(operador): aprendendo operadores de atribuicao.

--- estudos/index.php
+++ estudos/index.php
@@ -1,24 +1,32 @@
 <?php
-
-echo 'Ola mundo';
-
-/*Declarando variaveis.
-
-$name = 'Ana';
+echo 'Ola mundo' . PHP_EOL;
+//Declarando variaveis.
+/*$name = 'Ana';
 $age = 27;
 $result = ($age >= 18) ? 'Você é maior de idade.' : 'Você é menor de idade.';
 echo $result;
-*/
-
+//Arrays.
 $person1 = [
     'name' => 'Ana Flávia',
     'age' => 27,
     'trabalho' => true
 ];
-
-
 $person2 = [
     'name' => 'Agda Sousa',
     'age' => 20,
     'trabalho' => false
 ];
+//Operador de atribuição.
+$source = 20;
+$source /= 10;
+echo $source . PHP_EOL;
+$source2 = 50;
+$source2 -= 10;
+echo $source2 . PHP_EOL;
+$source3 = 30;
+$source3 += 5;
+echo $source3 . PHP_EOL;
+$source4 = 10;
+$source4 *= 10;
+echo $source4 . PHP_EOL;
+*/
